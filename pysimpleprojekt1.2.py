b1ebba29c745cd6d8b5c06807b3ecbbedfb3440f
vinay <136108213+VinayDesai-I@users.noreply.github.com>
2023-07-21 14:30:00 +0530

    Update pysimpleprojekt1.2.py

--- pysimpleprojekt1.2.py
+++ pysimpleprojekt1.2.py
@@ -1,20 +1,25 @@
 import PySimpleGUI as gui
-# GUI
+
 def gui_table():
+    
     infotable = []
     head = ['ID','Product Name','Quantity','Price']
+
     gui.theme("HotDogStand")
-    #WINDOW TO GET INPUT ID AND QUANTITY
+
     layout = [[gui.Text("Enter ID", size = (10,1)), gui.Input(key = "ID", do_not_clear = False)],
             [gui.Text("Enter Quantity", size = (10,1)), gui.Input(key = "QTY", do_not_clear = False)],
-            [gui.Button("ADD"), gui.Exit()],
-            [gui.Table(values = infotable, headings = head, key = "tablebill")]
+            [gui.Button("Add")],
+            [gui.Table(values = infotable, headings = head, key = "tablebill")],
+            [gui.Exit()]
         ]
+    
     window = gui.Window("KINDLY ENTER", layout)
+    
     while True:
         event, values = window.read()
         
-        if event == "ADD":
+        if event == "Add":
             
             data = [['banana',20,5],['bottles',200,4],['blah',10000,6]]
             price = int(values["QTY"]) * int(data[int(values["ID"])][2])
@@ -22,18 +27,7 @@ def gui_table():
             window["tablebill"].update(values = infotable)
 
         if event in (gui.WIN_CLOSED, "Exit"):
-            break   
-            # DATA IS table
-            #layout = [
-            #   [gui.Table(values = infotable, headings = head)],
-            #    [gui.Button("Exit")]
-            #    ]
-            #window = gui.Window("BILL",layout)
-            #while True:
-            #   event , values = window.read()
-            #    if event == "Exit" or event == gui.WIN_CLOSED:
-            #        break
-            #window.close()
+            break
     window.close()
  
 gui_table()
