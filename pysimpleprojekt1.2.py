7ff5d85b042cfb3c449d595325cd765e9fe8e64a
vinay <136108213+VinayDesai-I@users.noreply.github.com>
2023-07-21 19:47:27 +0530

    Update pysimpleprojekt1.2.py

--- pysimpleprojekt1.2.py
+++ pysimpleprojekt1.2.py
@@ -1,16 +1,16 @@
 import PySimpleGUI as gui
 
 def gui_table():
-    
+    price2 = []
     infotable = []
     head = ['ID','Product Name','Quantity','Price']
 
-    gui.theme("HotDogStand")
+    gui.theme("DarkAmber")
 
     layout = [[gui.Text("Enter ID", size = (10,1)), gui.Input(key = "ID", do_not_clear = False)],
             [gui.Text("Enter Quantity", size = (10,1)), gui.Input(key = "QTY", do_not_clear = False)],
             [gui.Button("Add")],
-            [gui.Table(values = infotable, headings = head, key = "tablebill")],
+            [gui.Text("Total Price:", size = (10,1)), gui.Text(" ", size = (10,1), key = "p")],
             [gui.Exit()]
         ]
     
@@ -26,6 +26,11 @@ def gui_table():
             infotable.append([values["ID"], data[int(values["ID"])][0],values["QTY"],price])
             window["tablebill"].update(values = infotable)
 
+        price = 0
+        for i in price2:
+            price+=i
+        window["p"].update(price)
+        
         if event in (gui.WIN_CLOSED, "Exit"):
             break
     window.close()
